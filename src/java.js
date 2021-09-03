cdfe714c1a1aac5505a6ac9d048c7d2761015662
Mags93 <88136424+Mags93@users.noreply.github.com>
2021-09-03 18:30:25 +0100

    Showing current temperature

---
+++ src/java.js
@@ -0,0 +1,24 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "ae2b4828efdd2e61b49f7cddc2e893a9";
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=ae2b4828efdd2e61b49f7cddc2e893a9&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
